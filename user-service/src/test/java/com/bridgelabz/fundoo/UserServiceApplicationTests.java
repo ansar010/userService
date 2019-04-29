6574fc20c51c74a83ceb842c987d50adcf65d94a
ansar010 <ansaruddeen030@gmail.com>
2019-04-29 20:43:56 +0530

    29April

--- user-service/src/test/java/com/bridgelabz/fundoo/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/bridgelabz/fundoo/UserServiceApplicationTests.java
@@ -2,13 +2,25 @@ package com.bridgelabz.fundoo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bridgelabz.fundoo.user.dao.IUserRepository;
+import com.bridgelabz.fundoo.user.service.IUserServices;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceApplicationTests {
 
+	@Autowired
+	private IUserServices userServices;
+	
+	@MockBean
+	private IUserRepository userRepository;
+
+	
 	@Test
 	public void contextLoads() {
 	}
